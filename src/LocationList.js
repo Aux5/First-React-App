df7d059c573bf34aa1eba0aff2d26278420ae3d3
Brett Shearin <brettcshearin@hotmail.com>
2018-07-11 11:36:02 -0500

    first commit

---
+++ src/LocationList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class LocationList extends Component {
+    render() {
+        return(
+            <article>
+                <h1>Location List</h1>
+                <section>123 Dog St</section>
+                <section>456 Canine Ln</section>
+                <section>789 Woof Av</section>
+                <section>1011 Ruff Cr</section>
+            </article>
+        );
+    }
+
+}
